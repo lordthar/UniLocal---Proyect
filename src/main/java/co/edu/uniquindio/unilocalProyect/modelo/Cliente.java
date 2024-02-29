84a6419485cdf50e46dad5348c55758d02cf5529
Miguel Garcia <100101000+lordthar@users.noreply.github.com>
2024-02-29 10:02:46 -0500

    Application.properties commit

--- src/main/java/co/edu/uniquindio/unilocalProyect/modelo/Cliente.java
+++ src/main/java/co/edu/uniquindio/unilocalProyect/modelo/Cliente.java
@@ -1,4 +1,16 @@
 package co.edu.uniquindio.unilocalProyect.modelo;
 
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("clientes")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+
 public class Cliente {
+
 }

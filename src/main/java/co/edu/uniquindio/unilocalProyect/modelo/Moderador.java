129a4c7ef7aa1cbabec4b2675a2487b8978c494d
migue <miguelitogarioso@hotmail.com>
2024-02-29 22:19:30 -0500

    Id y equals include En la clase moderador

--- src/main/java/co/edu/uniquindio/unilocalProyect/modelo/Moderador.java
+++ src/main/java/co/edu/uniquindio/unilocalProyect/modelo/Moderador.java
@@ -1,9 +1,12 @@
 package co.edu.uniquindio.unilocalProyect.modelo;
 
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 
+@Document("moderadores")
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString
@@ -11,5 +14,7 @@ import java.io.Serializable;
 @Getter
 @AllArgsConstructor
 public class Moderador extends Cuenta implements Serializable {
+    @Id
+    @EqualsAndHashCode.Include
     private String codigo;
 }

b92e1dc3f70394bf89438016fa90f030ccc03e0a
AlejoOsorio <112147774+AlejoOsorio@users.noreply.github.com>
2024-04-18 13:11:23 -0500

    Actualizacion

--- src/main/java/co/edu/uniquindio/unilocalProyect/dtos/LoginDTO.java
+++ src/main/java/co/edu/uniquindio/unilocalProyect/dtos/LoginDTO.java
@@ -6,6 +6,6 @@ import org.hibernate.validator.constraints.Length;
 
 public record LoginDTO(
         @NotBlank @Email String email,
-        @NotBlank @Length(max = 10, min = 8) String password
+        @NotBlank @Length(max = 20, min = 8) String password
 ) {
 }

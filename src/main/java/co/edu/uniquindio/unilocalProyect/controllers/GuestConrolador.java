718de1d1e983255fbfe6ed519158e6d07bd96b50
AlejoOsorio <112147774+AlejoOsorio@users.noreply.github.com>
2024-04-18 10:19:48 -0500

    Actualizacion
    Se agrego FiltroToken, y se creo el controlador de invitado

---
+++ src/main/java/co/edu/uniquindio/unilocalProyect/controllers/GuestConrolador.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.unilocalProyect.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/guests")
+@RequiredArgsConstructor
+public class GuestConrolador {
+
+}
